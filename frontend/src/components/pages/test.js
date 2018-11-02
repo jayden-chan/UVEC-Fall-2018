927c3c0392bb262a23d492743c80ee365880ee0d
Jayden Chan <jaydencn7@gmail.com>
2018-11-01 17:38:14 -0700

    Initialize frontend project structure

---
+++ frontend/src/components/pages/test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        This is a test here
+      </div>
+    );
+  }
+}
+
+export default Test;
