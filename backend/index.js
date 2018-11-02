f9dc3f7a53e320828ed3493479b3ed456af654f4
Jayden Chan <jaydencn7@gmail.com>
2018-11-01 17:56:53 -0700

    Add basic API endpoint & fix relative import error

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) =>  {
+  res.send('Hello World!');
+});
+
+app.get('/hello', (req, res) => {
+  res.status(200).send('Hello world!!');
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- frontend/src/components/pages/test.js
+++ frontend/src/components/pages/test.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class Test extends Component {
+
+  getTitle() {
+
+  }
+
   render() {
     return (
       <div className="container">
