fba85f692b35aa5a256e5abcaa6de91db1b7d7f8
Jayden Chan <jaydencn7@gmail.com>
2018-11-04 10:06:41 -0800

    Update backend for Heroku

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3001;
+const port = process.env.PORT | 3001;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>  {
   res.send('Hello World!');
 });
 
 app.get('/hello', (req, res) => {
+  res.status(200).send('Google');
+});
+
+app.post('/newmessage', (req, res) => {
+  // parse jSON
+  //
+  // add to messages
   res.status(200).send('Google');
 });
 
